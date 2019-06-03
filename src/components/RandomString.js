43d5118d633a760e88a3855ca1353da2f1447aba
Phuocnd <shishima21@gmail.com>
2019-06-04 00:38:26 +0700

    update add zero number forward

--- src/components/RandomString.js
+++ src/components/RandomString.js
@@ -14,6 +14,7 @@ class RandomString extends Component {
         this.toRef = React.createRef();
         this.typeRef = React.createRef();
         this.qtyRef = React.createRef();
+        this.chkbox = React.createRef();
         this.onRandomString = this.onRandomString.bind(this);
         this.onChangeLen = this.onChangeLen.bind(this);
         this.onChangeSelectBox = this.onChangeSelectBox.bind(this);
@@ -27,6 +28,8 @@ class RandomString extends Component {
         let to = Number(this.toRef.current.value);
         let type = this.typeRef.current.value;
         let qty = Number(this.qtyRef.current.value);
+        let chkbox = Number(this.chkbox.current.checked);
+        console.log(chkbox)
         let result = '';
         if (from === '' || to === '') {
             alert('Please enter From-To.');
@@ -43,9 +46,13 @@ class RandomString extends Component {
                     let number = Math.floor((Math.random() * ((to + 1) - from)) + from);
                     let numberLen = number.toString().length;
                     let toLen = to.toString().length;
-                    if (numberLen < toLen) {
-                        for (let i = numberLen; i < toLen; i++) {
-                            number = '0' + number;
+
+                    // Add 0 forward
+                    if (chkbox === 1) {
+                        if (numberLen < toLen) {
+                            for (let i = numberLen; i < toLen; i++) {
+                                number = '0' + number;
+                            }
                         }
                     }
                     result += prefix + number + '\r';
@@ -106,6 +113,11 @@ class RandomString extends Component {
                         <span className="input-group-text lable__input-width cloudy-knoxville-gradient" >To</span>
                     </div>
                     <input type="text" className="form-control" defaultValue="100" ref={this.toRef} onChange={this.onChangeLen} />
+                    <div className="input-group-prepend">
+                        <div className="input-group-text input__border-radius_right input__border-left">
+                            <input type="checkbox" defaultChecked ref={this.chkbox} disabled={(this.state.isDisable) ? true : ''} />
+                        </div>
+                    </div>
                 </div>
 
                 <div className="input-group mb-3">
